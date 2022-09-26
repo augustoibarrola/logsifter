71c52c634d50f87f5f0c53b9a212ae2baa7b7b17
augustoibarrola <augusto.t.ibarrola@gmail.com>
2022-09-26 16:52:28 -0400

    refactoring

--- src/main/java/com/ger/logsif/service/SifterService.java
+++ src/main/java/com/ger/logsif/service/SifterService.java
@@ -3,17 +3,17 @@ package com.ger.logsif.service;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,8 +25,9 @@ public class SifterService {
 
 	final Path fileStorageLocation;
 
-	/* Constructor
-	 *  
+	/*
+	 * Constructor
+	 * 
 	 */
 	public SifterService(LogFileProperties logfileProperties) {
 		this.fileStorageLocation = Paths.get(logfileProperties.getUploadRepo()).toAbsolutePath().normalize();
@@ -37,15 +38,16 @@ public class SifterService {
 //            throw new FileStorageException("Could not create the directory where the uploaded files will be stored.", ex);
 		}
 	}
-	
+
 	public HashMap<Integer, String> findKeyInFile(MultipartFile file, String key) throws FileNotFoundException {
 		HashMap<Integer, String> foundKeys = new HashMap<Integer, String>();
 
 		try {
-		this.useContainsMethodWithString(file, key);
+			this.useContainsMethodWithString(file, key);
+			this.testReader(file);
 			return foundKeys;
 
-		} catch (FileNotFoundException exception) {
+		} catch (IOException exception) {
 			exception.printStackTrace();
 		}
 
@@ -53,7 +55,9 @@ public class SifterService {
 	}
 
 	private void useContainsMethodWithString(MultipartFile file, String key) throws FileNotFoundException {
-
+		System.out.println();
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		System.out.println("useContainsMethodWithString");
 		try {
 
 			Path f = this.fileStorageLocation.resolve(StringUtils.cleanPath(file.getOriginalFilename()));
@@ -73,7 +77,9 @@ public class SifterService {
 				String line = logfileScanner.nextLine();
 				String readableLine = new String(line);
 
-				if (readableLine.contains(key)) { System.out.print(lines + " | " + readableLine + "\n");}
+				if (readableLine.contains(key)) {
+					System.out.print(lines + " | " + readableLine + "\n");
+				}
 
 				lines++;
 
@@ -81,7 +87,7 @@ public class SifterService {
 
 			System.out.println("Total lines: " + lines + "\n");
 			System.out.println("Keyword found on a total of " + foundOnLine + " lines\n");
-
+			System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 		} catch (FileNotFoundException exception) {
 			exception.printStackTrace();
 		}
@@ -95,7 +101,7 @@ public class SifterService {
 
 		}
 	}
-	
+
 	/*
 	 * NOT COMPLETE
 	 */
@@ -139,19 +145,40 @@ public class SifterService {
 //		}
 //
 //	}
-	
-	public void testReader(MultipartFile file) {
+
+	public void testReader(MultipartFile file) throws IOException {
 		
 		Path f = this.fileStorageLocation.resolve(StringUtils.cleanPath(file.getOriginalFilename()));
 		File logfile = new File(f.toString());
+		LineIterator lineIterator = FileUtils.lineIterator(logfile, "UTF-8");
 		
-		try(LineIterator it = FileUtils.lineIterator(logfile, "UTF-8")){
-			
-		} catch (IOException exception) {
-			exception.printStackTrace();
+		try {
+			while (lineIterator.hasNext()) {
+				List<String> lineArrList = testReaderHelper1(lineIterator);
+				for (int a = 0; a < lineArrList.size() - 1; a++) {
+//					System.out.println(lineArrList.get(a));
+					System.out.println(lineArrList.toString());
+				}
+
+			}
+			System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		} finally {
+			lineIterator.close();
+		}
+
+	}
+	
+	private List<String> testReaderHelper1(LineIterator lineIterator) {
+		String line = lineIterator.nextLine().trim();
+		char[] lineCharArr = line.toCharArray();
+		List<String> lineArrList = new ArrayList<String>(lineCharArr.length);
+		System.out.println(line);
+		for(int i = 0; i < lineCharArr.length - 1; i++) {
+			lineArrList.add(i, Character.toString(lineCharArr[i]));
 		}
 		
-		
+		return lineArrList;
+
 	}
 
 }
